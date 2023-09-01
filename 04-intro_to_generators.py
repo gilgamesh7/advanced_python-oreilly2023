ed1d9d44a4f99505b3d365adca3fce4ad26c5a70
gilgamesh7 <felidaesrule@yahoo.com>
2023-09-01 20:19:14 +1200

    Intro to generators begin

---
+++ 04-intro_to_generators.py
@@ -0,0 +1,12 @@
+def my_function():
+    return None
+
+def my_generator():
+    yield
+    return
+
+print(f'{my_function  = }')
+print(f'{my_function()  = }') # Results
+
+print(f'{my_generator  = }')
+print(f'{my_generator()  = }') # Intermediation for getting results
