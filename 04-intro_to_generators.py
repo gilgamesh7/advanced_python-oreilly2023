0f7fc1cda26d1b164a8c8552e49d6b177cc8ab88
gilgamesh7 <felidaesrule@yahoo.com>
2023-10-02 19:12:42 +1300

    More generator examples

--- 04-intro_to_generators.py
+++ 04-intro_to_generators.py
@@ -10,3 +10,19 @@ print(f'{my_function()  = }') # Results
 
 print(f'{my_generator  = }')
 print(f'{my_generator()  = }') # Intermediation for getting results
+
+print(f'\n\n\n')
+
+def my_function_1( data, *, mode):
+    if mode:
+        return data ** 2
+    
+    return data ** 3
+
+def my_generator_2(data, *, mode):
+    if mode:
+        yield data ** 2
+    
+    yield data ** 3
+
+print(f"{my_function_1(3,mode=True) = }")
