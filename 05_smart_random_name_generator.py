cd231a179c8c9a9c071ffdeccc240cfb8335648f
gilgamesh7 <felidaesrule@yahoo.com>
2023-10-15 10:13:51 +1300

    Create classmethod for random name generator

--- 05_smart_random_name_generator.py
+++ 05_smart_random_name_generator.py
@@ -3,3 +3,16 @@ from string import ascii_lowercase
 from random import Random
 
 rnd = Random(0)
+
+class Entity(namedtuple('EntityBase','name value')):
+    @classmethod
+    def from_random(cls, *, random_state=None):
+        random_state = random_state if random_state is not None else Random()
+        return cls(
+            name = ''.join(rnd.choices(ascii_lowercase, k=4)),
+            value = rnd.randint(-1_000, +1_000),
+        )
+
+class Analysis:
+    def load(self):
+        self.data= [ ]
